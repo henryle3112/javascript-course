61e71797a5d14e7b0d5896379f87e82a0b3abdf3
henryle3112 <hieplt.developer@gmail.com>
2019-12-24 22:18:32 +0700

    Upload NodeJS source code

--- nodejs/employee_management/server.js
+++ nodejs/employee_management/server.js
@@ -3,10 +3,13 @@
 // body-parser
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
 
 const dbConnect = mysql.createConnection({
     host: "localhost",
@@ -16,12 +19,75 @@ const dbConnect = mysql.createConnection({
     database: "employee_management"
 });
 
-app.get('/employees', () => {
-    console.log('list of employees');
+app.get('/employees', (req, res) => {
+    // const employees = [
+    //     {
+    //         id: 1,
+    //         name: 'David',
+    //         age: 18,
+    //         salary: 500
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Jack',
+    //         age: 18,
+    //         salary: 600
+    //     },
+    //     {
+    //         id: 1,
+    //         name: 'Helios',
+    //         age: 18,
+    //         salary: 700
+    //     }
+    // ];
+
+    let sqlGetEmployees = 'SELECT * FROM employee';
+
+    dbConnect.query(sqlGetEmployees, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send(result);
+    });
+
+    // res.send(employees);
 })
 
-app.post('/employees', () => {
-    console.log('add new employee');
+app.get("/employees/:id", (req, res) => {
+    const selectedEmployeeId = req.params.id;
+    const sqlGetEmployeeById = `SELECT * FROM employee WHERE id = ${selectedEmployeeId}`;
+    dbConnect.query(sqlGetEmployeeById, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send(result);
+    })
+});
+
+app.post('/employees', (req, res) => {
+    // res.send(req.body);
+    const sqlAddEmployee = `INSERT INTO employee (name, age, salary) VALUES ?`;
+    const newEmployee = [
+        [
+            req.body.name,
+            req.body.age,
+            req.body.salary
+        ]
+    ]
+    dbConnect.query(sqlAddEmployee, [newEmployee], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send(result);
+    });
+});
+
+app.put('/employees', () => {
+    console.log('update employee');
+});
+
+app.delete('/employees', () => {
+    console.log('delete employee');
 })
 
 dbConnect.connect(err => {
